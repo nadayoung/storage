7b154bb165db1ee2f7df31ec3c98dcdfa32a5c97
da0 <da0@ewhain.net>
2024-02-16 15:46:54 +0900

    Video change

--- completeScreen.py
+++ completeScreen.py
@@ -80,8 +80,7 @@ def main(page: Page):
                 ElevatedButton(
                     "Save file",
                     icon=icons.SAVE,
-                    on_click={save_video_url(video.playlist[0].resource),
-                              upload_github()}
+                    on_click=lambda event: [save_video_url(video.playlist[0].resource), upload_github()]
                 ),
             ]
         ),
