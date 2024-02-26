4b1ae98b4057e2675892596e8be9085426c04528
bribri0067 <bribri0067@gmail.com>
2024-02-26 16:30:51 +0900

    project assets update

--- front_copy.py
+++ front_copy.py
@@ -492,13 +492,17 @@ def main(page: Page):
                             controls=[
                                 video,
                                 Column(
-                                    width=300,
-                                    alignment=MainAxisAlignment.CENTER,
-                                    controls=[
-                                        ElevatedButton("Save file", icon=icons.SAVE, on_click=lambda _: save_trimmed_file, width=200),
-                                        Container(),
-                                        ElevatedButton("돌아가기", on_click=lambda _: page.go("/"), width=200, bgcolor=colors.INDIGO_ACCENT_700, color=colors.WHITE),
-                                    ],
+                                    Container(
+                                        Column(
+                                            width=400,
+                                            alignment=CrossAxisAlignment.CENTER,
+                                            controls=[
+                                                ElevatedButton("Save file", icon=icons.SAVE, on_click=lambda _: save_trimmed_file, width=200),
+                                                Container(),
+                                                ElevatedButton("돌아가기", on_click=lambda _: page.go("/"), width=200, bgcolor=colors.INDIGO_ACCENT_700, color=colors.WHITE),
+                                            ]
+                                        )
+                                    )
                                 ),
                             ]
                         ),
